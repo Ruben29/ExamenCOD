4d95223f3880cae52bac2e6bc631f450ffa281fc
rfernandezgonzalez <rfernandezgonzalez@eniac04.danielcastelao.org>
2014-12-04 09:27:12 +0100

    He vuelto al commit inicial hecho por Damián!

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -1,3 +1,4 @@
+
 package examencod;
 
 public class ExamenCOD {
@@ -6,30 +7,26 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int inicio = 11;
-        if (metodoRuben(inicio)) {
-            System.out.println("Es numero primo");
-        } else {
-            System.out.println("NO es numero primo");
-        }
-
-        for (int var2 = 2; var2 < 1000; var2++) {
-            if (metodoRuben(var2)) {
-                System.out.print(var2 + " ");
-            }
-        }
-    }
-
-    public static boolean metodoRuben(int var3) {
-        int varMet = 2;
-        boolean auxMet = true;
-        while ((auxMet) && (varMet != var3)) {
-            if (var3 % varMet == 0) {
-                auxMet = false;
-            }
-            varMet++;
-        }
-        return auxMet;
-    }
-
+ int iN = 11;
+            	if (metodoMio(iN))
+System.out.println("Es numero primo");
+else
+ System.out.println("NO es numero primo");           	 
+          	 
+for (int x=2;x<1000;x++) {
+if (metodoMio(x))
+System.out.print(x + " ");                         	 
+}         	 
+	}
+public static boolean metodoMio(int variable){
+int j = 2;
+boolean aux=true;
+while ((aux) && (j!=variable)){
+if (variable % j == 0)
+aux = false;
+j++;
+}
+return aux;
+  } 	 
+    
 }
